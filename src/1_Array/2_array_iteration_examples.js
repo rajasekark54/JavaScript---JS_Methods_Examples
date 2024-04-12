1bf4a591217b08380931c272c2b699289fe57aa8
Rajasekar <rajasekark54@gmail.com>
2024-04-12 08:24:03 +0530

    feature: updated the readme file         - changes applied to variable names for better understanding

--- src/1_Array/2_array_iteration_examples.js
+++ src/1_Array/2_array_iteration_examples.js
@@ -1,32 +1,32 @@
 // Iteration way 1
-const array = [1, 2, 3, 4, 5];
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
+const array1 = [1, 2, 3, 4, 5];
+for (let i = 0; i < array1.length; i++) {
+  console.log(array1[i]);
 }
 
 // Iteration way 2
 console.log('\n');
-const array1 = [1, 2, 3, 4, 5];
-array1.forEach((item) => {
+const array2 = [1, 2, 3, 4, 5];
+array2.forEach((item) => {
   console.log(item);
 });
 
 // Iteration way 3
 console.log('\n');
-const array2 = [1, 2, 3, 4, 5];
-for (const item of array2) {
+const array3 = [1, 2, 3, 4, 5];
+for (const item of array3) {
   console.log(item);
 }
 
 // Iteration way 4
 console.log('\n');
-const array3 = [1, 2, 3, 4, 5];
-for (const key in array3) {
-  console.log(array3[key]);
+const array4 = [1, 2, 3, 4, 5];
+for (const key in array4) {
+  console.log(array4[key]);
 }
 
 // Iteration way 4
 console.log('\n');
-const array4 = [1, 2, 3, 4, 5];
-const newArray = array4.map((item) => item * 2);
+const array5 = [1, 2, 3, 4, 5];
+const newArray = array5.map((item) => item * 2);
 console.log(newArray);
--- src/3_Object/2_object_iteration_examples.js
+++ src/3_Object/2_object_iteration_examples.js
@@ -1,31 +1,31 @@
 // Iteration way 1
-const obj = { a: 1, b: 2, c: 3 };
-for (const key in obj) {
-  console.log(`${key}: ${obj[key]}`);
+const obj1 = { a: 1, b: 2, c: 3 };
+for (const key in obj1) {
+  console.log(`${key}: ${obj1[key]}`);
 }
 
 // Iteration way 2
-const obj1 = { a: 1, b: 2, c: 3 };
-Object.keys(obj1).forEach((key) => {
-  console.log(`${key}: ${obj1[key]}`);
+const obj2 = { a: 1, b: 2, c: 3 };
+Object.keys(obj2).forEach((key) => {
+  console.log(`${key}: ${obj2[key]}`);
 });
 
 // Iteration way 3
-const obj2 = { a: 1, b: 2, c: 3 };
-Object.values(obj2).forEach((value) => {
+const obj3 = { a: 1, b: 2, c: 3 };
+Object.values(obj3).forEach((value) => {
   console.log(value);
 });
 
 // Iteration way 4
-const obj3 = { a: 1, b: 2, c: 3 };
-Object.entries(obj3).forEach(([key, value]) => {
+const obj4 = { a: 1, b: 2, c: 3 };
+Object.entries(obj4).forEach(([key, value]) => {
   console.log(`${key}: ${value}`);
 });
 
 // Iteration way 5
-const obj4 = { a: 1, b: 2, c: 3 };
-for (const key in obj4) {
-  if (obj4.hasOwnProperty(key)) {
-    console.log(`${key}: ${obj4[key]}`);
+const obj5 = { a: 1, b: 2, c: 3 };
+for (const key in obj5) {
+  if (obj5.hasOwnProperty(key)) {
+    console.log(`${key}: ${obj5[key]}`);
   }
 }
